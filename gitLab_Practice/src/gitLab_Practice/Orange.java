b5ab695ad65130a40dae5ef9bfde9b915599952b
91878 <91878@Jesus.Dlink>
2023-02-19 18:19:39 +0530

    First commit
    Uploading file

--- gitLab_Practice/src/gitLab_Practice/Orange.java
+++ gitLab_Practice/src/gitLab_Practice/Orange.java
@@ -1,16 +1,5 @@
 package gitLab_Practice;
 
 public class Orange {
-String color;
-Orange(String color)
-{
-this.color=color;
-}
-public static void main(String[] args) {
-// TODO Auto-generated method stub
-Orange greenApple = new Orange("green");
-String color = greenApple.getcolor();
-System.out.println("color of apple is &quot" + color);
 
-}
 }
