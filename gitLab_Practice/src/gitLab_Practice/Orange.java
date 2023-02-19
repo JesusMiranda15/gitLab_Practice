471e587d3c8d0abc3423b0829a00aea40b230aea
91878 <91878@Jesus>
2023-02-19 19:43:40 +0530

    Hello

--- gitLab_Practice/src/gitLab_Practice/Orange.java
+++ gitLab_Practice/src/gitLab_Practice/Orange.java
@@ -1,22 +1,5 @@
 package gitLab_Practice;
 
-
-
 public class Orange {
-String color;
-Orange(String color)
-{
-this.color=color;
-}
-public static void main(String[] args) {
-// TODO Auto-generated method stub
-Orange greenApple = new Orange("green");
-String color = greenApple.getcolor();
-System.out.println("color of apple is " + color);
 
-}
-private String getcolor() {
-	// TODO Auto-generated method stub
-	return null;
-}
 }
